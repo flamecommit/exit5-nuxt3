e67e41967be1454f4366caf1c36bc5c73bd18e44
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-05-30 13:23:43 +0900

    .

--- pages/music/[...slug].vue
+++ pages/music/[...slug].vue
@@ -1,19 +1,28 @@
 <script setup>
-const { path, query } = useRoute();
+const { path, query, params } = useRoute();
 
-const { data } = await useAsyncData('music', () => queryContent('/music').where({_path: path}).findOne());
-
-watch(() => path, () => {
-  console.log('change path');
-  refreshNuxtData('music');
+const state = reactive({
+  singer: null,
+  score: null,
 });
-const score = data.value.score;
 
-console.log(score, path)
+/*const { data: music } = await useAsyncData('music', async () => queryContent('/music').where({_path: path}).findOne());
+
+console.log(music);*/
+
+// console.log(music.value.singer);
+
+const { data: music } = await useFetch(() => queryContent('/music').where({_path: path}).findOne());
+
+/*defineExpose({
+  ...toRefs(state),
+})*/
 </script>
 
 <template>
   <div class="page-music">
-    <score-chord :score="score" />
+    {{ music }}
+    <!-- <p>가수 : {{ music.singer }}</p>
+    <score-chord :score="music.score" /> -->
   </div>
 </template>
